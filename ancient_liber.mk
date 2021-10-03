3c2a4371d4f44f9582411065c57ad9068ca5c05c
menorziin <williamsilvapppl@gmail.com>
2021-10-03 08:11:12 +0000

    liber: Official AncientOS
    Change-Id: I1d4fb98da11734d3c66ba1afc2a950212b91d0c1

--- ancient_liber.mk
+++ ancient_liber.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from liber device
 $(call inherit-product, device/motorola/liber/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Ancient stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_liber
+PRODUCT_NAME := ancient_liber
 PRODUCT_DEVICE := liber
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Motorola One Fusion+
@@ -53,3 +53,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
+
+# AncientOS
+ANCIENT_GAPPS := false
+ANCIENT_OFFICIAL := true
