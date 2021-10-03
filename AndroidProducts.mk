25e834845b7b5e0f7c5912e83f5770b06e532f51
menorziin <williamsilvapppl@gmail.com>
2021-10-03 08:11:12 +0000

    liber: Official AncientOS
    Change-Id: I1d4fb98da11734d3c66ba1afc2a950212b91d0c1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_liber.mk
+    $(LOCAL_DIR)/ancient_liber.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_liber-eng \
-    lineage_liber-user \
-    lineage_liber-userdebug
+    ancient_liber-eng \
+    ancient_liber-user \
+    ancient_liber-userdebug
